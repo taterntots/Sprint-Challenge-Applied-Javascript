03e96ec93e2ad0bdf1c5cc3d2fc553372db731b9
taterntots <matthew.a.weidner@gmail.com>
2019-11-01 13:14:14 -0400

    Solved the card dilemma! API successfully called and cards created. MVP achieved.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,7 +1,7 @@
 // STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
+// Study the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
@@ -20,8 +20,29 @@
 
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
     .then(response => {
-        console.log(response);
+        // console.log(response.data.articles);
+        for(let key in response.data.articles) {
+            // console.log(id);
+            for(let i = 0; i < response.data.articles[key].length; i++) {
+                console.log(response.data.articles[key]);
+                const entryPoint = document.querySelector('.cards-container');
+                const articleCard = cardCreator(response.data.articles[key][i]);
+                // console.log(response.data.articles[key][i]);
+                entryPoint.appendChild(articleCard);
+            }
+        }
     })
+
+// axios.get('https://lambda-times-backend.herokuapp.com/articles')
+//     .then(response => {
+//         // console.log(response);
+//         Object.entries(response.data.articles).forEach(article => {
+//             console.log(article);
+//             const entryPoint = document.querySelector('cards-container');
+//             const articleCard = cardCreator(article);
+//             entryPoint.appendChild(articleCard);
+//         })
+//     })
 
 function cardCreator(object) {
     const newCard = document.createElement('div');
@@ -41,7 +62,7 @@ function cardCreator(object) {
     author.appendChild(imgContainer);
 
     const imgAuthor = document.createElement('img');
-    imgAuthor.textContent = object.authorPhoto;
+    imgAuthor.src = object.authorPhoto;
     imgContainer.appendChild(imgAuthor);
 
     const authorsName = document.createElement('span');
